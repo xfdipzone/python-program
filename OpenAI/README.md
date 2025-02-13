843dd3880a2cb7ba89c05a4fc40f371bc4e1146b
xfdipzone <xfdipzone@gmail.com>
2025-02-13 10:38:39 +0800

    feat: add OpenAI customer service

--- OpenAI/README.md
+++ OpenAI/README.md
@@ -19,6 +19,16 @@
 
 [01. AI 写情信](./write_letter.py)
 
+让 AI 写一封情信
+
 [02. AI 查询产品价格范围](./product_price_range.py)
 
+让 AI 预测一件产品的销售价格范围
+
 [03. AI 情感分析评论](./sentiment_analysis.py)
+
+让 AI 判断评论是好评还是差评
+
+[04. AI 客服](./customer_service.py)
+
+让 AI 根据给出的提示，返回客服回答用户的内容
---
+++ OpenAI/customer_service.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from openai import OpenAI
+import os
+import time
+
+client = OpenAI(api_key = os.environ.get("OPENAI_API_KEY"))
+
+COMPLETION_MODEL = "gpt-4o-mini"
+
+prompt = '请你用朋友的语气回复给到客户，并称他为“亲”，他的订单已经发货在路上了，预计在3天之内会送达，订单号2025YEAS，我们很抱歉因为天气的原因物流时间比原来长，感谢他选购我们的商品。'
+
+def get_response(prompt, temperature = 1.0, stop=None):
+    completions = client.chat.completions.create(
+        model=COMPLETION_MODEL, # 模型
+        messages=[{"role": "user", "content": prompt}], # 提示词
+        max_tokens=1024, # 最大可用的 token 数，包括提示词与返回内容的用量
+        n=1, # 返回 1 个结果，如果是写作有关的可以调整为 3，返回多个文案
+        stop=stop, # 遇到指定字符停止输出
+        temperature=temperature, # 控制输出内容随机性，范围 0 ~ 2.0，越大表示随机性越好
+    )
+    message = completions.choices[0].message
+    return message.content
+
+print("第一次回答：" + get_response(prompt, 0.5))
+time.sleep(1)
+print("第二次回答：" + get_response(prompt, 1.0))
+time.sleep(1)
+print("第三种回答：" + get_response(prompt, 0.0))
