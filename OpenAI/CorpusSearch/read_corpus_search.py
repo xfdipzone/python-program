9fda23bf6ac1ccfe3ba9ec2b6f3380e6668e7a26
xfdipzone <xfdipzone@gmail.com>
2025-03-05 20:30:23 +0800

    feat: update OpenAI corpus search
    add local embedding model.

--- OpenAI/CorpusSearch/read_corpus_search.py
+++ OpenAI/CorpusSearch/read_corpus_search.py
@@ -5,19 +5,34 @@
 dependency packages
 pip install llama-index
 pip install langchain
+pip install transformers
+pip install sentence-transformers
+pip install llama-index-embeddings-huggingface
+pip install llama-index-llms-huggingface
+pip install icetk
+pip install cpm_kernels
+pip install torch>=2.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 
 此方法需要 OpenAI Quota
 """
 import openai
 import os
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.huggingface import HuggingFaceLLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModel
 
 # 设置 OpenAI API 密钥
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+# 定义 embedding 模型
+embed_model = HuggingFaceEmbedding(
+    model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+)
+
 # 加载数据并构建索引
 documents = SimpleDirectoryReader('./data/mr_fujino').load_data()
-index = VectorStoreIndex.from_documents(documents)
+index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
 
 # 持久化索引到磁盘
 index.storage_context.persist(persist_dir='index_mr_fujino')
