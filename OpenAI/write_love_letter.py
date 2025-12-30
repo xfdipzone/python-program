efcbd18ac8eefad87a7afb559647893fddeaf37e
xfdipzone <xfdipzone@gmail.com>
2025-12-30 19:37:37 +0800

    refactor: update OpenAI write love letter

--- OpenAI/write_love_letter.py
+++ OpenAI/write_love_letter.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from openai import OpenAI
-import os
+from google.colab import userdata
 
 """
 AI 写情信
@@ -8,16 +8,16 @@ AI 写情信
 dependency packages
 pip install openai
 """
-client = OpenAI(
-    api_key=os.environ['OPENAI_API_KEY'],
-)
+client = OpenAI(api_key=userdata.get("OPENAI_API_KEY"))
+
+COMPLETION_MODEL = "gpt-4o-mini"
 
 completion = client.chat.completions.create(
-    model="gpt-4o-mini",
+    model=COMPLETION_MODEL,
     store=False,  # true: 保存结果 false: 不保存结果
     messages=[
         {"role": "user", "content": "write a lover letter about ai, use chinese language."}
     ]
 )
 
-print(completion.choices[0].message)
+print(completion.choices[0].message.content)
