fd4d7682fb63fff8c2a7d05334d6e6f1509b91cd
xfdipzone <xfdipzone@gmail.com>
2026-01-10 14:37:33 +0800

    refactor: update OpenAI GPT models encoding

--- OpenAI/gpt_models_encoding.py
+++ OpenAI/gpt_models_encoding.py
@@ -8,7 +8,9 @@ dependency packages
 pip install tiktoken
 """
 
-# 获取 OpenAI GPT Model 对应的 TikToken 编码
-model = "gpt-4o-mini"
-encoding = tiktoken.encoding_for_model(model)
-print("Model: %s\nTikToken Encoding: %s" % (model, encoding.name))
+# 获取 OpenAI GPT Models 对应的 TikToken 编码
+models = ["gpt-4o-mini", "gpt-4o", "gpt-3.5-turbo", "davinci-002", "gpt-5"]
+
+for model in models:
+    encoding = tiktoken.encoding_for_model(model)
+    print("Model: %s\nTikToken Encoding: %s\n" % (model, encoding.name))
