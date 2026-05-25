cb0d28d40f109043e2c635a2199ca1f348162a98
xfdipzone <xfdipzone@gmail.com>
2026-05-25 14:39:25 +0800

    refactor: update zero shot classification Faiss similarity product search
    support batch search.

--- AI/Embedding/ZeroShotClassification/faiss_similarity_product_search.py
+++ AI/Embedding/ZeroShotClassification/faiss_similarity_product_search.py
@@ -24,9 +24,9 @@ login(token=userdata.get("HF_TOKEN"))
 # 使用 SentenceTransformer 加载计算文本向量模型
 embed_model = SentenceTransformer("google/embeddinggemma-300M")
 
-# 计算文本的向量 (embedding)
-def get_embedding(text):
-    return embed_model.encode(text)
+# 计算多个文本的向量 (embedding)
+def get_embeddings(list_of_texts, batch_size=32):
+    return embed_model.encode(list_of_texts, batch_size=batch_size)
 
 
 # 将产品数据保存到 Faiss
@@ -47,41 +47,51 @@ def load_embeddings_to_faiss(df):
 
 
 # 搜索产品
-def search_product(index, df, query, n=3, threshold=0.5):
+def search_product(index, df, queries, n=3, threshold=0.5):
     # 要搜索的产品名称的 Embedding，并归一化处理
-    query_vector = np.array(get_embedding(query)).astype(
-        "float32").reshape(1, -1)
-    faiss.normalize_L2(query_vector)
+    query_vectors = np.array(get_embeddings(queries)).astype("float32")
+    faiss.normalize_L2(query_vectors)
 
-    # 搜索，找出最接近的 n 个产品
-    scores, indexes = index.search(query_vector, n)
+    # 批量搜索，找出最接近的 n 个产品
+    scores, indexes = index.search(query_vectors, n)
 
-    # 构建结果
-    results = pd.DataFrame(
-        {
-            "product_name": df.iloc[indexes[0]]["product_name"].values,
-            "similarity": scores[0]
-        }
-    )
+    # 构建结果，dict(query=>search results)
+    results_dict = {}
 
-    # 过滤相似度，只保留大于 threshold 的结果
-    results = results[results["similarity"] > threshold]
-    results = results.sort_values("similarity", ascending=False)
+    for i, query in enumerate(queries):
+        results = pd.DataFrame(
+            {
+                "id": df.iloc[indexes[i]].index.values,
+                "product_name": df.iloc[indexes[i]]["product_name"].values,
+                "similarity": scores[i]
+            }
+        )
 
-    return results
+        # 过滤相似度，只保留大于 threshold 的结果
+        results = results[results["similarity"] > threshold]
+        results = results.sort_values("similarity", ascending=False)
+
+        results_dict[query] = results
+
+    return results_dict
 
 
 # 打印结果
-def print_results(results):
-    if results.empty:
-        print(f"没有找到相似的产品")
-    else:
-        print(f"找到以下产品：")
-        for idx, row in results.iterrows():
-            name = row["product_name"]
-            score = row["similarity"]
-            print(f"标号：{idx:2}｜相似度：{score:.4f}｜产品名称：{name}")
-    print("-" * 80)
+def print_results(results_dict):
+    # 遍历结果字典键值对
+    for query, results in results_dict.items():
+        print(f"搜索：{query}")
+
+        if results.empty:
+            print(f"没有找到相似的产品")
+        else:
+            print(f"找到以下产品：")
+            for _, row in results.iterrows():
+                id = row["id"]
+                name = row["product_name"]
+                score = row["similarity"]
+                print(f"产品编号：{id:2}｜相似度：{score:.4f}｜产品名称：{name}")
+        print("-" * 80)
 
 
 # 读取 parquet 文件到 Data Frame
@@ -94,12 +104,21 @@ index = load_embeddings_to_faiss(df)
 # 相似度阈值
 threshold_value = 0.5
 
-query = "优雅自然女背包"
-results = search_product(index, df, query, n=3, threshold=threshold_value)
-print(f"搜索：{query}")
-print_results(results)
+queries = ["优雅自然女背包", "高颜值高性能手机"]
+results_dict = search_product(index, df, queries, 3, threshold_value)
+print_results(results_dict)
 
-query = "高颜值高性能手机"
-results = search_product(index, df, query, n=3, threshold=threshold_value)
-print(f"搜索：{query}")
-print_results(results)
+"""
+搜索：优雅自然女背包
+找到以下产品：
+产品编号：55｜相似度：0.5790｜产品名称：【特卖专场】精美刺绣手提包 优雅女性首选 全场满减
+产品编号：81｜相似度：0.5165｜产品名称：【新品首发】运动风背包 时尚休闲 买就送小礼品
+产品编号：87｜相似度：0.5040｜产品名称：【限时抢购】流苏装饰斜挎包 时尚新潮 限时优惠
+--------------------------------------------------------------------------------
+搜索：高颜值高性能手机
+找到以下产品：
+产品编号： 6｜相似度：0.5706｜产品名称：【抢先体验】Realme GT2 Pro 12G+256G 5G高性能手机
+产品编号： 1｜相似度：0.5633｜产品名称：【疯狂抢购】华为Mate 40 Pro 8G+256G 5G旗舰手机
+产品编号：11｜相似度：0.5506｜产品名称：【特价促销】三星Note20 Ultra 12G+512G 5G手机
+--------------------------------------------------------------------------------
+"""
