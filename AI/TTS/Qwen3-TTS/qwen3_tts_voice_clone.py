2210cc1de3e9d94085152057e7629f55351b8749
xfdipzone <xfdipzone@gmail.com>
2026-07-16 23:19:41 +0800

    feat: add base on Qwen3-TTS implements voice clone text to speech

---
+++ AI/TTS/Qwen3-TTS/qwen3_tts_voice_clone.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+import soundfile as sf
+import os
+from qwen_tts import Qwen3TTSModel
+from IPython.display import Audio, display, Markdown
+
+"""
+文本转为克隆得语音（基于 Qwen3-TTS）
+使用用户提供的音色文件提取特征
+
+模型：
+https://huggingface.co/Qwen/Qwen3-TTS-12Hz-1.7B-Base
+3.9 GB，显存需求 6-8 GB，推理速度较慢，音质更自然，更稳定，情绪表达丰富，长文本稳定性高，Voice Clone 相似度更高
+
+https://huggingface.co/Qwen/Qwen3-TTS-12Hz-0.6B-Base
+1.8 GB，显存需求 2-4 GB，推理速度很快，音质很好，情绪表达一般，长文本稳定性弱，Voice Clone 相似度较高
+
+dependency packages
+pip install -U qwen-tts
+pip install soundfile
+pip install ipython
+"""
+# 加载模型
+tts = Qwen3TTSModel.from_pretrained(
+    "Qwen/Qwen3-TTS-12Hz-1.7B-Base",
+    device_map="cuda"
+)
+
+# 参考语音源
+source_sound = "reference.wav"
+
+# 文本内容
+text = "王总您好，提醒您一下，今天下午两点在三号会议室有一个关于新项目的推进会，各部门经理都会出席。会议资料我已经打印好放在您的办公桌左侧了，另外，五点钟您和李总还有一个视频会议。"
+
+# 保存语音的目录
+output_dir = "data/output_audio"
+
+# 检查保存的目录，如不存在则创建目录
+os.makedirs(output_dir, exist_ok=True)
+
+audio = tts.generate_voice_clone(
+    text=text,
+    language="chinese",
+    ref_audio=source_sound,
+    ref_text="支付宝到账500万元。",
+    max_new_tokens=1024,
+    temperature=0.3,
+    top_k=10,
+    repetition_penalty=1.05
+)
+
+waveform = audio[0][0]
+sample_rate = audio[1]
+
+# 语音文件
+file_path = os.path.join(output_dir, "voice_clone.wav")
+
+# 保存语音
+sf.write(
+    file_path,
+    waveform,
+    sample_rate
+)
+
+display(Markdown(f"### 🎙️ **Voice Clone**"))
+display(Markdown(f"语音文件已自动保存到： `{file_path}`"))
+display(Audio(waveform, autoplay=False, rate=sample_rate))
